8d46ec4043da4747ba48c2a590f05297366934ff
Aniket Munim <aniketmunim@anikets-air.mynetworksettings.com>
2023-01-06 15:20:32 -0500

    server

--- server/server.js
+++ server/server.js
@@ -98,7 +98,7 @@ if (process.env.NODE_ENV === 'production') {
 
     app.get('/', (req, res) => {
         app.use(express.static(path.join(__dirname, '../client', 'build')))
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'))
     })
 }
 
